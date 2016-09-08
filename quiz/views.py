114ab7777e019938e452c61ecf75081990b4456b
popcornbrain <tina.elvian@gmail.com>
2016-09-08 12:52:19 +0200

    added some data to try "quizzes" in views

--- quiz/views.py
+++ quiz/views.py
@@ -1,5 +1,29 @@
 from django.shortcuts import render
 
+quizzes = [
+		{
+				"quiz_number": 1, 
+				"name": "Klassiska böcker",
+				"description": "Hur bra kan du dina klassiker?"
+		},
+
+
+		{
+				"quiz_number": 2, 
+				"name": "Största 1slagen", 
+				"description": "Kan du dina lag?"
+		},
+
+
+		{
+				"quiz_number": 3, 
+				"name": "Världens mest kända hackare",
+				"description": "Hackerhistoria är viktigt, kan du den?"
+		},
+
+			]
+
+
 def startpage(request):
 	return render(request, "quiz/startpage.html")
 
