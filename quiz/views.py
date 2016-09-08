ef4f71203412fcca2b7cc5d3615e2720b38265cd
popcornbrain <tina.elvian@gmail.com>
2016-09-08 15:08:18 +0200

    #14 update in quiztemplate

--- quiz/views.py
+++ quiz/views.py
@@ -25,10 +25,24 @@ quizzes = [
 
 
 def startpage(request):
-	return render(request, "quiz/startpage.html")
 
-def quiz(request):
-	return render(request, "quiz/quiz.html")
+	context = {
+			"quizzes": quizzes,
+			}
+
+	return render(request, "quiz/startpage.html", context)
+
+
+
+def quiz(request, quiz_number):
+
+	context = {
+			"quiz": quizzes[int(quiz_number) - 1],
+
+			"quiz_number": quiz_number, 
+			}
+	return render(request, "quiz/quiz.html", context)
+
 
 def question(request):
 	return render(request, "quiz/question.html")
