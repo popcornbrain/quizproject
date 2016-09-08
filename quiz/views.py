e8cbe0a398ee84059458bc47d31d86b6449cfe0a
popcornbrain <tina.elvian@gmail.com>
2016-09-08 15:47:04 +0200

    #15 added some info on question template

--- quiz/views.py
+++ quiz/views.py
@@ -28,7 +28,7 @@ def startpage(request):
 
 	context = {
 			"quizzes": quizzes,
-			}
+			  }
 
 	return render(request, "quiz/startpage.html", context)
 
@@ -40,12 +40,22 @@ def quiz(request, quiz_number):
 			"quiz": quizzes[int(quiz_number) - 1],
 
 			"quiz_number": quiz_number, 
-			}
+			  }
 	return render(request, "quiz/quiz.html", context)
 
 
-def question(request):
-	return render(request, "quiz/question.html")
+def question(request, quiz_number, question_number):
+	context = {
+			"question_number": question_number, 
+			"question": "Hur många bultar har ölandsbron?", 
+
+			"answer1": "12", 
+			"answer2": "66 400", 
+			"answer3": "7 428 954", 
+			"quiz_number": quiz_number, 
+		      }
+	return render(request, "quiz/question.html", context)
+
 
 def completed(request):
 	return render(request, "quiz/completed.html")
