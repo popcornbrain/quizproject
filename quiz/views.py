30d54eb18d553b4c5c1bd7c6e99bc1e99e5c78b0
popcornbrain <tina.elvian@gmail.com>
2016-09-09 11:55:07 +0200

    changes in views.py

--- quiz/views.py
+++ quiz/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from quiz.models import Quiz
 
 quizzes = [
 		{
@@ -26,9 +27,10 @@ quizzes = [
 
 def startpage(request):
 
-	context = {
-			"quizzes": quizzes,
-			  }
+	context = context = {
+            "quizzes": Quiz.objects.all(),
+    }
+
 
 	return render(request, "quiz/startpage.html", context)
 
