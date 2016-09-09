2d199a61d4196b0de1ea9df19a67b320d31ff7ec
popcornbrain <tina.elvian@gmail.com>
2016-09-09 14:46:05 +0200

    DONE!

--- quiz/views.py
+++ quiz/views.py
@@ -62,21 +62,37 @@ def question(request, quiz_number, question_number):
 
 
 def answer(request, quiz_number, question_number):
-    saved_answers = request.session.get(quiz_number, {})
-    answer = int(request.POST["answer"])
-    saved_answers[question_number] = answer
-    request.session[quiz_number] = saved_answers
+	saved_answers = request.session.get(quiz_number, {})
+	answer = int(request.POST["answer"])
+	saved_answers[question_number] = answer
+	request.session[quiz_number] = saved_answers
 
-    question_number = int(question_number)
-    return redirect("question_page", quiz_number, question_number + 1)
+	question_number = int(question_number)
+	quiz = Quiz.objects.get(quiz_number = quiz_number)
+	num_questions = quiz.questions.count()
+	if num_questions <= question_number:
+		return redirect("completed_page", quiz_number)
+	else:
+		return redirect("question_page", quiz_number, question_number + 1)
+
 
 
 def completed(request, quiz_number):
+	quiz = Quiz.objects.get(quiz_number=quiz_number)
+	questions=quiz.questions.all()
+	saved_answers=request.session.get(quiz_number,{})
+
+	num_correct_answers = 0
+	for question_number, answer in saved_answers.items():
+		correct_answer=questions[int(question_number)-1].correct
+		if correct_answer==answer:
+			num_correct_answers+=1
+
+
 	context = {
-			"correct": 12, 
-			"total": 20, 
-			"quiz_number": quiz_number,
-			  }
+		"correct": num_correct_answers, 
+		"total": questions.count(), 
+			}
 	return render(request, "quiz/completed.html", context)
 
 
