f1dc7ca8926221ad91d4781c23f01106c02dec84
popcornbrain <tina.elvian@gmail.com>
2016-09-09 10:39:43 +0200

    added django admin and include

--- quizsite/urls.py
+++ quizsite/urls.py
@@ -1,3 +1,6 @@
+from django.contrib import admin
+from django.conf.urls import include
+
 from django.conf.urls import url
 from quiz import views
 
@@ -6,5 +9,5 @@ urlpatterns = [
 		url(r"^quiz/([0-9]+)/$", views.quiz, name="quiz_page"), 
 		url(r"^quiz/([0-9]+)/question/([0-9]+)/$", views.question, name="question_page"),
 		url(r"^quiz/([0-9]+)/completed/$", views.completed, name="completed_page"),
-
+		url(r"^admin/", include(admin.site.urls))
 ]
