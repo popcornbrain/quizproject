481ca1b46aafc74b6b9e34a85d8114469f541b60
popcornbrain <tina.elvian@gmail.com>
2016-09-09 10:18:22 +0200

    Two models added in quizproject/quiz/models.py

--- quiz/models.py
+++ quiz/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Quiz(models.Model):
+		quiz_number=models.PositiveIntegerField()
+		name=models.CharField(max_lenght=100)
+		description=models.TextField()
+
+class Question(models.Model):
+		question=models.TextField()
+		answer1=models.CharField(max_lenght=100)
+		answer2=models.CharField(max_lenght=100)
+		answer3=models.CharField(max_lenght=100)
+		quiz=models.ForeignKey(Quiz, related_name="questions")
+
