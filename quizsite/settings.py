1ef0639a948c2d39a86c254b8f5189cfa79c27ef
popcornbrain <tina.elvian@gmail.com>
2016-09-09 15:11:31 +0200

    Lost

--- quizsite/settings.py
+++ quizsite/settings.py
@@ -75,12 +75,11 @@ WSGI_APPLICATION = 'quizsite.wsgi.application'
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.config(
+        default='sqlite:////{0}'.format(os.path.join(BASE_DIR,
+'db.sqlite3'))
+    )
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
@@ -123,3 +122,4 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
     )
+STATIC_ROOT = 'staticfiles'
