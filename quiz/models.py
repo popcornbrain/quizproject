192cc3d91267519ba346516e12f1b4ef7c415d83
popcornbrain <tina.elvian@gmail.com>
2016-09-09 10:21:52 +0200

    updated version on added models

--- quiz/models.py
+++ quiz/models.py
@@ -1,14 +1,15 @@
 from django.db import models
 
 class Quiz(models.Model):
-		quiz_number=models.PositiveIntegerField()
-		name=models.CharField(max_lenght=100)
-		description=models.TextField()
+    quiz_number = models.PositiveIntegerField()
+    name = models.CharField(max_length=100)
+    description = models.TextField()
 
 class Question(models.Model):
-		question=models.TextField()
-		answer1=models.CharField(max_lenght=100)
-		answer2=models.CharField(max_lenght=100)
-		answer3=models.CharField(max_lenght=100)
-		quiz=models.ForeignKey(Quiz, related_name="questions")
+    question = models.TextField()
+    answer1 = models.CharField(max_length=100)
+    answer2 = models.CharField(max_length=100)
+    answer3 = models.CharField(max_length=100)
+    correct = models.PositiveIntegerField()
+    quiz = models.ForeignKey(Quiz, related_name="questions")
 
